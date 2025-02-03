e18aac50069f36c6c743fc5900b54606628702f5
TreNor's .ID <128129543+startdaz@users.noreply.github.com>
2025-02-03 23:43:53 +0700

    Update debug.py

--- caligo/modules/debug.py
+++ caligo/modules/debug.py
@@ -26,7 +26,6 @@ class Debug(module.Module):
             return f"__Command__ `{cmd_name}` __doesn't exist.__"
 
         src = await util.run_sync(inspect.getsource, self.bot.commands[cmd_name].func)
-        # Strip first level of indentation
         filtered_src = re.sub(r"^ {4}", "", src, flags=re.MULTILINE)
 
         await ctx.respond(
@@ -92,7 +91,7 @@ class Debug(module.Module):
 
     @command.desc("Evaluate code")
     @command.usage("[code snippet]")
-    @command.alias("exec")
+    @command.alias("e")
     async def cmd_eval(self, ctx: command.Context) -> Optional[str]:
         code = ctx.input
         if not code:
@@ -101,20 +100,15 @@ class Debug(module.Module):
         out_buf = io.StringIO()
 
         async def _eval() -> Tuple[str, Optional[str]]:
-            # Message sending helper for convenience
             async def send(*args: Any, **kwargs: Any) -> pyrogram.types.Message:
                 return await ctx.msg.reply(*args, **kwargs)
 
-            # Print wrapper to capture output
-            # We don't override sys.stdout to avoid interfering with other output
             def _print(*args: Any, **kwargs: Any) -> None:
                 if "file" not in kwargs:
                     kwargs["file"] = out_buf
-
                 return print(*args, **kwargs)
 
             eval_vars = {
-                # Contextual info
                 "self": self,
                 "ctx": ctx,
                 "bot": self.bot,
@@ -124,23 +118,18 @@ class Debug(module.Module):
                 "listeners": self.bot.listeners,
                 "modules": self.bot.modules,
                 "stdout": out_buf,
-                # Convenience aliases
                 "context": ctx,
                 "msg": ctx.msg,
                 "message": ctx.msg,
                 "db": self.bot.db,
-                # Helper functions
                 "send": send,
                 "print": _print,
-                # Built-in modules
                 "inspect": inspect,
                 "os": os,
                 "re": re,
                 "sys": sys,
                 "traceback": traceback,
-                # Third-party modules
                 "pyrogram": pyrogram,
-                # Bot
                 "command": command,
                 "module": module,
                 "util": util,
@@ -148,31 +137,14 @@ class Debug(module.Module):
 
             try:
                 return "", await meval(code, globals(), **eval_vars)
-            except Exception as e:  # skipcq: PYL-W0703
-                # Find first traceback frame involving the snippet
-                first_snip_idx = -1
-                tb = traceback.extract_tb(e.__traceback__)
-                for i, frame in enumerate(tb):
-                    if frame.filename == "<string>" or frame.filename.endswith(
-                        "ast.py"
-                    ):
-                        first_snip_idx = i
-                        break
-
-                # Re-raise exception if it wasn't caused by the snippet
-                if first_snip_idx == -1:
-                    raise e
-
-                # Return formatted stripped traceback
-                stripped_tb = tb[first_snip_idx:]
-                formatted_tb = util.error.format_exception(e, tb=stripped_tb)
-                return "⚠️ Error executing snippet\n\n", formatted_tb
+            except Exception as e:
+                tb = traceback.format_exception(e)
+                return "⚠️ Error executing snippet\n\n", "\n".join(tb)
 
         before = util.time.usec()
         prefix, result = await _eval()
         after = util.time.usec()
 
-        # Always write result if no output has been collected thus far
         if not out_buf.getvalue() or result is not None:
             print(result, file=out_buf)
 
@@ -180,26 +152,18 @@ class Debug(module.Module):
         el_str = util.time.format_duration_us(el_us)
 
         out = out_buf.getvalue()
-        # Strip only ONE final newline to compensate for our message formatting
         if out.endswith("\n"):
             out = out[:-1]
 
-        respond_text = f"""{prefix}<b>Input</b>:
+        respond_text = f"""
+<b>⚡ Code Evaluation ⚡</b>
+
+<b>🔹 Input:</b>
 <pre language="python">{escape(code)}</pre>
-<b>Output</b>:
+
+<b>🔸 Output:</b>
 <pre language="python">{escape(out)}</pre>
-<b>Time</b>:
-<pre>{escape(el_str)}</pre>"""
-        if len(respond_text) > 4096:
-            with io.BytesIO(str.encode(out)) as out_file:
-                out_file.name = "eval.text"
-                await ctx.msg.reply_document(
-                    document=out_file, caption=code, disable_notification=True
-                )
 
-            return None
-
-        await ctx.respond(
-            respond_text,
-            parse_mode=pyrogram.enums.parse_mode.ParseMode.HTML,
-        )
+<b>⏱️ Execution Time:</b> {escape(el_str)}
+"""
+        await ctx.respond(respond_text, parse_mode=pyrogram.enums.parse_mode.ParseMode.HTML)
